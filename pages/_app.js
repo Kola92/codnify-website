72babbb0bf06cf1bb63baa0cbf3b9e8e79de910a
Adekola Olawale <rolawale92@gmail.com>
2022-04-07 18:38:20 +0100

    modified eslintrc.json

--- pages/_app.js
+++ pages/_app.js
@@ -18,12 +18,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content={Component.description} />
         
       </Head>
-      {/* jQuery */}
-      <Script
-          src="https://code.jquery.com/jquery-3.6.0.min.js"
-          integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-          crossOrigin="anonymous"
-        ></Script>
+      
       <Component {...pageProps} />;
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document() {
   return (
     <Html>
@@ -46,6 +45,13 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
         />
+
+        {/* jQuery */}
+        <script
+          src="https://code.jquery.com/jquery-3.6.0.min.js"
+          integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
