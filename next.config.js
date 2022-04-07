607383cf80569be525ed785aa2dba84121e3d976
Adekola Olawale <rolawale92@gmail.com>
2022-04-07 18:05:49 +0100

    modified webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
-// const withImages = require("next-images");
-const webpack = require("webpack");
+// const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
   reactStrictMode: true
 }
 
-
-
-module.exports = withPlugins({
-    webpack: (config) => {
-      config.node = {
-        fs: "empty",
-      };
-      config.module.rules.push({
-        test: /\.(glb|gltf|bin)$/,
-        use: {
-          loader: "file-loader",
-        },
-      });
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery",
-          "window.jQuery": "jquery",
-        })
-      );
-      return config;
-    },
-  }, nextConfig);
+// module.exports = withPlugins([
+//     webpack: (config) => {
+//       config.node = {
+//         fs: "empty",
+//       };
+//       config.module.rules.push({
+//         test: /\.(glb|gltf|bin)$/,
+//         use: {
+//           loader: "file-loader",
+//         },
+//       });
+//       config.plugins.push(
+//         new webpack.ProvidePlugin({
+//           $: "jquery",
+//           jQuery: "jquery",
+//           "window.jQuery": "jquery",
+//         })
+//       );
+//       return config;
+//     },
+// ], nextConfig);
 
 
 
@@ -58,4 +54,4 @@ module.exports = withPlugins({
 //   },
 // });
 
-// module.exports = nextConfig
+module.exports = nextConfig;
